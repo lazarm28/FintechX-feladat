e6f49cce051d407daff77d7f3645839a910d33b0
Mate Lazar <lazar.mate97@gmail.com>
2020-05-21 08:41:02 +0200

    add files

---
+++ kong_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo -n "
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1beta1
+metadata:
+  name: cluster-admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: User
+  name: lazar.mate97@gmail.com # usually the Google account
+                                         # eg: harry@konghq.com
+  namespace: kube-system" | kubectl apply -f -
+
+export PROXY_IP=$(kubectl get -o jsonpath="{.status.loadBalancer.ingress[0].ip}" service -n kong kong-proxy)
+curl -i $PROXY_IP
+kubectl apply -f https://bit.ly/echo-service
+
+echo "
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: demo
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /foo
+        backend:
+          serviceName: echo
+          servicePort: 80
+" | kubectl apply -f -
+ingress.extensions/demo created
+
+curl -i $PROXY_IP/foo
