e08d4cf1d342f83971451fd1e02b76cc02c014ab
Mate Lazar <lazar.mate97@gmail.com>
2020-05-21 14:08:03 +0200

    update files

--- kong_setup.sh
+++ kong_setup.sh
@@ -18,7 +18,7 @@ subjects:
 export PROXY_IP=$(kubectl get -o jsonpath="{.status.loadBalancer.ingress[0].ip}" service -n kong kong-proxy)
 curl -i $PROXY_IP
 kubectl apply -f https://bit.ly/echo-service
-
+sleep 15
 echo "
 apiVersion: extensions/v1beta1
 kind: Ingress
@@ -34,5 +34,5 @@ spec:
           servicePort: 80
 " | kubectl apply -f -
 ingress.extensions/demo created
-
+sleep 15
 curl -i $PROXY_IP/foo
